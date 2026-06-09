a24bdd7c5887ade86a28beb7353d13cf4b8131a6
Lokesh Prajapati <lokeshprajapati565374@gmail.com>
2026-06-09 17:43:53 +0530

    Find duplicate with slow and fast pointer

---
+++ Array/2 Sum/Find_Duplicate_using_slow_and_fast_pointer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int> &nums)
+{
+    int slow = nums[0], fast = nums[0];
+    do
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    slow = nums[0];
+    while(slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
+
+int main()
+{
+    vector<int> nums = {3,1,3,4,2};
+    int duplicate = findDuplicate(nums);
+    cout << "Duplicate Element is : " << duplicate << endl;
+    return 0;
+}
